a78581f0d4b43acde0d56ced6680b3b7183fcff9
defabricated <defabricated@gmail.com>
2018-12-02 11:57:41 +0100

    Return captchaId after successful solve request; add ReportBadCaptha function to report invalid captcha responses

--- twocaptcha.go
+++ twocaptcha.go
@@ -37,16 +37,54 @@ func New(apiKey string) *TwoCaptchaClient {
 }
 
 // SolveRecaptchaV2 performs a recaptcha v2 solving request to 2captcha.com
-// and returns with the solved captcha if the request was successful.
+// and returns with the solved captcha and captcha ID if the request was successful.
 // Valid ApiKey is required.
 // See more details on https://2captcha.com/2captcha-api#solving_recaptchav2_new
-func (c *TwoCaptchaClient) SolveRecaptchaV2(siteURL, recaptchaKey string) (string, error) {
+func (c *TwoCaptchaClient) SolveRecaptchaV2(siteURL, recaptchaKey string) (string, string, error) {
 	captchaId, err := c.apiRequest(
 		ApiURL,
 		map[string]string{
 			"googlekey": recaptchaKey,
 			"pageurl":   siteURL,
 			"method":    "userrecaptcha",
+		},
+		0,
+		3,
+	)
+
+	if err != nil {
+		return "", "", err
+	}
+
+	resp, err := c.apiRequest(
+		ResultURL,
+		map[string]string{
+			"googlekey": recaptchaKey,
+			"pageurl":   siteURL,
+			"method":    "userrecaptcha",
+			"id":        captchaId,
+			"action":    "get",
+		},
+		5,
+		20,
+	)
+	return resp, captchaId, err
+}
+
+// SolveRecaptchaV3 performs a recaptcha v3 solving request to 2captcha.com
+// and returns with the solved captcha if the request was successful.
+// Valid ApiKey is required.
+// See more details on https://2captcha.com/solving_recaptcha_v3
+func (c *TwoCaptchaClient) SolveRecaptchaV3(siteURL, recaptchaKey, action string, minScore float64) (string, error) {
+	captchaId, err := c.apiRequest(
+		ApiURL,
+		map[string]string{
+			"googlekey": recaptchaKey,
+			"pageurl":   siteURL,
+			"method":    "userrecaptcha",
+			"version":   "v3",
+			"action":    action,
+			"min_score": fmt.Sprintf("%.1f", minScore),
 		},
 		0,
 		3,
@@ -70,41 +108,18 @@ func (c *TwoCaptchaClient) SolveRecaptchaV2(siteURL, recaptchaKey string) (strin
 	)
 }
 
-// SolveRecaptchaV3 performs a recaptcha v3 solving request to 2captcha.com
-// and returns with the solved captcha if the request was successful.
-// Valid ApiKey is required.
-// See more details on https://2captcha.com/solving_recaptcha_v3
-func (c *TwoCaptchaClient) SolveRecaptchaV3(siteURL, recaptchaKey, action string, minScore float64) (string, error) {
-	captchaId, err := c.apiRequest(
-		ApiURL,
+func (c *TwoCaptchaClient) ReportBadCaptcha(captchaId string) (error) {
+	_, err := c.apiRequest(
+		ResultURL,
 		map[string]string{
-			"googlekey": recaptchaKey,
-			"pageurl":   siteURL,
-			"method":    "userrecaptcha",
-			"version": "v3",
-			"action": action,
-			"min_score": fmt.Sprintf("%.1f", minScore),
+			"id":     captchaId,
+			"action": "reportbad",
 		},
 		0,
 		3,
 	)
 
-	if err != nil {
-		return "", err
-	}
-
-	return c.apiRequest(
-		ResultURL,
-		map[string]string{
-			"googlekey": recaptchaKey,
-			"pageurl":   siteURL,
-			"method":    "userrecaptcha",
-			"id":        captchaId,
-			"action":    "get",
-		},
-		5,
-		20,
-	)
+	return err
 }
 
 func (c *TwoCaptchaClient) apiRequest(URL string, params map[string]string, delay time.Duration, retries int) (string, error) {
@@ -136,7 +151,7 @@ func (c *TwoCaptchaClient) apiRequest(URL string, params map[string]string, dela
 	if strings.Contains(string(body), "CAPCHA_NOT_READY") {
 		return c.apiRequest(URL, params, delay, retries-1)
 	}
-	if !strings.Contains(string(body), "OK|") {
+	if (params["action"] == "reportbad" && string(body) != "OK_REPORT_RECORDED") || (params["action"] != "reportbad" && !strings.Contains(string(body), "OK|")) {
 		return "", errors.New("Invalid respponse from 2captcha: " + string(body))
 	}
 	return string(body[3:]), nil
